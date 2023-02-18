c0e0cb26dea5650e23ddf949a21e8998a97081cd
taka2 <taka23131@gmail.com>
2023-02-18 13:46:57 +0900

    条件式の評価の追加

--- src/eval.rs
+++ src/eval.rs
@@ -2,10 +2,15 @@ use crate::ast::Expr;
 #[derive(Eq, PartialEq, Debug, Clone)]
 pub enum Value {
     VInt(i64),
+    VBool(bool),
 }
 
 fn v_int(n: i64) -> Value {
     Value::VInt(n)
+}
+
+fn v_bool(b: bool) -> Value {
+    Value::VBool(b)
 }
 
 pub struct Eval {}
@@ -14,7 +19,7 @@ impl Eval {
     pub fn new() -> Eval {
         Eval {}
     }
-    pub fn eval_expr(&self, ast: Expr) -> Result<Value, &str> {
+    pub fn eval_expr(&self, ast: Expr) -> Result<Value, String> {
         match ast {
             Expr::EBinOp(op, e1, e2) => {
                 let v1 = self.eval_expr(*e1)?;
@@ -25,11 +30,19 @@ impl Eval {
                         "-" => Ok(v_int(n1 - n2)),
                         "*" => Ok(v_int(n1 * n2)),
                         "/" => Ok(v_int(n1 / n2)),
-                        _ => Err("unimplemented operator"),
+                        "<" => Ok(v_bool(n1 < n2)),
+                        ">" => Ok(v_bool(n1 > n2)),
+                        "<=" => Ok(v_bool(n1 <= n2)),
+                        ">=" => Ok(v_bool(n1 >= n2)),
+                        "==" => Ok(v_bool(n1 == n2)),
+                        "!=" => Ok(v_bool(n1 != n2)),
+                        _ => Err(format!("unimplemented operator {}", op)),
                     },
+                    (_, _) => Err("type error".to_string()),
                 }
             }
             Expr::EInt(n) => Ok(v_int(n)),
+            Expr::EIf(cond, e1, e2) => unimplemented!(),
         }
     }
 }
@@ -37,11 +50,17 @@ impl Eval {
 #[test]
 fn test_eval_expr() {
     use crate::parser::parser_expr;
-    fn test_eval_expr_helper(str: &str, v: Result<Value, &str>) {
+    fn test_eval_expr_helper(str: &str, v: Result<Value, String>) {
         let eval = Eval::new();
         assert_eq!(eval.eval_expr(parser_expr(str).unwrap().1), v)
     }
     test_eval_expr_helper("3*3+4*4", Ok(v_int(25)));
     test_eval_expr_helper("4+(6/3)-2", Ok(v_int(4)));
-    test_eval_expr_helper("2+4/2/2", Ok(v_int(3)))
+    test_eval_expr_helper("2+4/2/2", Ok(v_int(3)));
+    test_eval_expr_helper("3>2", Ok(v_bool(true)));
+    test_eval_expr_helper("3<2", Ok(v_bool(false)));
+    test_eval_expr_helper("2>=2", Ok(v_bool(true)));
+    test_eval_expr_helper("2<=3", Ok(v_bool(true)));
+    test_eval_expr_helper("2==3", Ok(v_bool(false)));
+    test_eval_expr_helper("2!=3", Ok(v_bool(true)));
 }
