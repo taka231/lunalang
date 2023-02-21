37e5862509acbd9553be880a6a85f6f4dfe8b70f
taka2 <taka23131@gmail.com>
2023-02-21 20:23:41 +0900

    replで式を評価する前に型推論をするようにした; :t <expr>で式の型を確認出来るようにした

--- src/repl.rs
+++ src/repl.rs
@@ -1,6 +1,7 @@
 use crate::ast::Expr;
 use crate::eval::{Eval, Value};
 use crate::parser::parser_expr;
+use crate::typeinfer::typeinfer_expr;
 use std::io;
 pub fn repl() {
     println!("Welcome to lunalang repl!");
@@ -13,9 +14,23 @@ pub fn repl() {
         if program == ":q\n" {
             break;
         }
+        let mut is_typecheck = false;
+        if program.starts_with(":t") {
+            program = program[2..].to_string();
+            is_typecheck = true;
+        }
         let ast = parser_expr(&program);
         match ast {
             Ok((_, ast)) => {
+                let ty = typeinfer_expr(&ast);
+                if let Err(err) = ty {
+                    println!("type error: {}", err);
+                    continue;
+                }
+                if is_typecheck {
+                    println!("{:?}", ty.unwrap());
+                    continue;
+                }
                 let result = eval.eval_expr(ast);
                 println!("{:?}", result);
             }
