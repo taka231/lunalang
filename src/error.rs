f7cc4a9c193cad863dd070181f2edfa1f5a9493b
taka2 <taka23131@gmail.com>
2023-02-22 00:33:30 +0900

    TypeInferError型とEvalError型のDisplayトレイトを実装

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,6 @@
 use crate::typeinfer::Type;
+use std::fmt;
+use std::fmt::Display;
 
 pub trait Error {}
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -7,8 +9,26 @@ pub enum TypeInferError {
     OccurError(u64, Type),
 }
 
+impl Display for TypeInferError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::UnifyError(t1, t2) => write!(f, "Cannot unify {} to {}", t1, t2),
+            Self::OccurError(n, t) => write!(f, "{} is occur in type {}", n, t),
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum EvalError {
     InternalTypeError,
     UnimplementedOperatorError(String),
+}
+
+impl Display for EvalError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InternalTypeError => write!(f, "InternalTypeError"),
+            Self::UnimplementedOperatorError(op) => write!(f, "{} is unimplemented", op),
+        }
+    }
 }
