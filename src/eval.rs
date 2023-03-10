706a423d8710983260a64e91dbec4fdc8b82b3f6
taka2 <taka23131@gmail.com>
2023-03-10 22:46:05 +0900

    束縛文を評価するメソッドの追加

--- src/eval.rs
+++ src/eval.rs
@@ -1,4 +1,6 @@
-use crate::ast::Expr;
+use std::collections::HashMap;
+
+use crate::ast::{Expr, Statement, Statements};
 use crate::error::EvalError;
 #[derive(Eq, PartialEq, Debug, Clone)]
 pub enum Value {
@@ -14,11 +16,35 @@ fn v_bool(b: bool) -> Value {
     Value::VBool(b)
 }
 
-pub struct Eval {}
+pub struct Environment {
+    env: HashMap<String, Value>,
+}
+
+impl Environment {
+    pub fn new() -> Self {
+        Environment {
+            env: HashMap::new(),
+        }
+    }
+    pub fn get(&self, name: String) -> Result<Value, EvalError> {
+        match self.env.get(&name) {
+            Some(value) => Ok(value.clone()),
+            None => Err(EvalError::UndefinedVariable(name)),
+        }
+    }
+    pub fn insert(&mut self, name: String, val: Value) {
+        self.env.insert(name, val);
+    }
+}
+
+pub struct Eval {
+    env: Environment,
+}
 
 impl Eval {
     pub fn new() -> Eval {
-        Eval {}
+        let env = Environment::new();
+        Eval { env }
     }
     pub fn eval_expr(&self, ast: Expr) -> Result<Value, EvalError> {
         match ast {
@@ -53,6 +79,23 @@ impl Eval {
             }
         }
     }
+    pub fn eval_statement(&mut self, ast: Statement) -> Result<(), EvalError> {
+        match ast {
+            Statement::Assign(name, e) => {
+                let val = self.eval_expr(e)?;
+                Ok(self.env.insert(name, val))
+            }
+        }
+    }
+    pub fn eval_statements(&mut self, asts: Statements) -> Result<(), EvalError> {
+        for ast in asts {
+            self.eval_statement(ast)?;
+        }
+        Ok(())
+    }
+    pub fn eval_main(&self) -> Result<Value, EvalError> {
+        self.env.get("main".to_string())
+    }
 }
 
 #[test]
@@ -74,4 +117,15 @@ fn test_eval_expr() {
     test_eval_expr_helper("if (3>2) 1 else 2", Ok(v_int(1)));
     test_eval_expr_helper("if (3<2) 1 else 2", Ok(v_int(2)));
     test_eval_expr_helper("if (3<2) 1 else if (4==4) 2 else 3", Ok(v_int(2)));
+}
+
+#[test]
+fn test_eval_statements() {
+    use crate::parser::parser_statements;
+    fn test_eval_statements_helper(str: &str, v: Result<Value, EvalError>) {
+        let mut eval = Eval::new();
+        eval.eval_statements(parser_statements(str).unwrap().1);
+        assert_eq!(eval.eval_main(), v)
+    }
+    test_eval_statements_helper("let main = 4;", Ok(v_int(4)));
 }
